58bcad7a0c9727406dcccb48d843cc94c4bdd79e
nhurst <nahurst@gmail.com>
2013-03-17 18:34:13 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HappyTo::Application.config.secret_token = 'c77ccb89e6ff3472ad045cc7bea8d4b2e7475523cb0abdac2a84f144df35d63aeae7336e51fb1ebd4b2ecd39d0f146084312a2faebd118aee6ee33d1c9f45e10'
