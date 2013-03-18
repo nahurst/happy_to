e0db60399e911f3a094d8defa159bdffedf02a1e
nhurst <nahurst@gmail.com>
2013-03-17 20:26:59 -0400

    Setting up basic javascript framework

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,18 +1,45 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery
 //= require jquery_ujs
 //= require foundation
 //= require_tree .
 
 $(document).foundation();
+var APP = APP || {};
+APP = {
+  common: {
+    init: function() {
+
+      alert("init");
+    },
+  },
+  mail_merges: {
+    'new': function() {
+      alert("new");
+
+    }
+  },
+}
+
+UTIL = {
+  exec : function(controller, action){
+    var ns = APP,
+    action = (action === undefined) ? "init" : action;
+
+    if (controller !== "" &&
+      ns[controller] &&
+      typeof ns[controller][action] === "function" ) {
+      ns[controller][action]();
+    }
+  },
+  init: function() {
+    var body = document.body,
+    controller = body.getAttribute("data-controller"),
+    action = body.getAttribute("data-action");
+    UTIL.exec("common");
+    UTIL.exec(controller);
+    UTIL.exec(controller, action);
+  }
+};
+$(document).ready(UTIL.init);
+
+
